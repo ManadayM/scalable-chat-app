45388122dc0b9b97ac1ca79c2783930bc6ecb066
Manaday Mavani <manadaym@users.noreply.github.com>
2023-12-19 23:50:12 +0530

    web: adds basic chat UI

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,28 @@
 "use client";
+import { useState } from "react";
+import { useSocket } from "../context/SocketProvider";
+import css from "./page.module.css";
 
-export default function Page() { 
+export default function Page() {
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState("");
   
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <div>
+      <div>
+        <h1>All messages</h1>
+      </div>
+      <div>
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          className={css["chat-input"]}
+          type="text"
+          placeholder="Message.."
+        />
+        <button onClick={(e) => sendMessage(message)} className={css["button"]}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
 }
