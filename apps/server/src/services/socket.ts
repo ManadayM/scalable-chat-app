6bdd0554e165ff7169176b9a70284bdda98e1e8c
Manaday Mavani <manadaym@users.noreply.github.com>
2023-12-20 01:34:47 +0530

    server: adds redis pub-sub model to scale

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,18 @@
 import { Server } from 'socket.io';
+import Redis from 'ioredis';
+
+const pub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 25072,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD
+});
+const sub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 25072,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD
+});
 
 class SocketService {
   private _io: Server;
@@ -11,19 +25,30 @@ class SocketService {
         origin: '*'
       }
     });
+
+    sub.subscribe('MESSAGES');
   }
 
   public initListeners() {
     const io = this.io;
     console.log('Init socket listeners...');
-    
+
     io.on('connect', (socket) => {
       console.log(`New socket connected: ${socket.id}`);
 
       socket.on('event:message', async ({ message }: { message: string }) => {
         console.log(`New message received: ${message}`);
+        // Now, publish this message to redis.
+        await pub.publish('MESSAGES', JSON.stringify({ message }))
       });
     });
+
+    sub.on('message', (channel, message) => {
+      if (channel === 'MESSAGES') {
+        console.log('New msg from redis', message);
+        io.emit("message", message);
+      }
+    })
   }
 
   get io() {
